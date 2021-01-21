117de265b9a2f6c802038a7411c1603e5a965f9c
SizeR <vip.sanki007@bk.ru>
2021-01-21 21:45:30 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AccessoryTestClassLibrary;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Accessory accessory = new Accessory();
+            Assert.IsTrue(accessory.checkType("R"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Accessory accessory = new Accessory();
+            Assert.IsTrue(accessory.changeType("R"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Accessory accessory = new Accessory();
+            Assert.IsTrue(accessory.checkValue(50000));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Accessory accessory = new Accessory();
+            Assert.IsTrue(accessory.changeValue(50000));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Accessory accessory = new Accessory();
+            Assert.IsTrue(accessory.checkQuantity(10));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Accessory accessory = new Accessory();
+            Assert.IsTrue(accessory.changeQuantity(10));
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Accessory accessory = new Accessory();
+            Assert.AreEqual(accessory.whatType(), "Резистор");
+        }
+    }
+}
